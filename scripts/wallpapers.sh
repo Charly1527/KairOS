d65c1bddb22250c97d98f523809a1960e09c422c
Charly1527 <carlisdanielvazjim@gmail.com>
2026-01-15 05:03:10 -0600

    test  1.15.5.3

--- scripts/wallpapers.sh
+++ scripts/wallpapers.sh
@@ -4,20 +4,23 @@ set -e
 echo "==> Instalando fondos de pantalla de KairOS..."
 
 DEST="/usr/share/backgrounds/kairos"
+XML_DIR="/usr/share/gnome-background-properties"
+
 mkdir -p "$DEST"
+mkdir -p "$XML_DIR"
 
 cp /root/KairOS/branding/wallpapers/* "$DEST/"
 
-cat > /usr/share/gnome-background-properties/kairos.xml <<EOF
+cat > "$XML_DIR/kairos.xml" <<EOF
 <?xml version="1.0"?>
 <!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
 <wallpapers>
   <wallpaper>
     <name>KairOS Default</name>
-    <filename>$DEST/olimpo-dark.png</filename>
+    <filename>$DEST/kairos-default.jpg</filename>
     <options>zoom</options>
   </wallpaper>
 </wallpapers>
 EOF
 
-echo "==> Fondos instalados correctamente"
+echo "==> Fondos de pantalla de KairOS instalados correctamente"
